7980667107aba94700cfafb0360cfc9407501162
Andy Jones <hello@awj.digital>
2018-09-29 16:10:06 +0100

    updates to prototype

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,125 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.get('/user/index', function (req, res) {
+    req.session.clear
+    res.render('user/index')
+})
+
+router.post('/user/page1', function (req, res) {
+    if (req.session.data['cya'] === 1) {
+        res.redirect('/user/check')
+    } else {
+        res.redirect('/user/page2')
+    }
+
+});
+router.post('/user/page2', function (req, res) {
+    if (req.session.data['cya'] === 1) {
+        res.redirect('/user/check')
+    } else {
+        res.redirect('/user/page3')
+    }
+
+});
+router.post('/user/page3', function (req, res) {
+    if (req.session.data['cya'] === 1) {
+        res.redirect('/user/check')
+    } else {
+        res.redirect('/user/page4')
+    }
+
+});
+router.post('/user/page4', function (req, res) {
+    if (req.session.data['cya'] === 1) {
+        res.redirect('/user/check')
+    } else {
+        res.redirect('/user/page5')
+    }
+
+});
+router.post('/user/page5', function (req, res) {
+    if (req.session.data['cya'] === 1) {
+        res.redirect('/user/check')
+    } else {
+        res.redirect('/user/page6')
+    }
+
+});
+router.post('/user/page6', function (req, res) {
+    if (req.session.data['cya'] === 1) {
+        res.redirect('/user/check')
+    } else {
+        res.redirect('/user/page7')
+    }
+
+});
+router.post('/user/page6a', function (req, res) {
+    res.redirect('/user/page6b')
+
+});
+router.post('/user/page6b', function (req, res) {
+    if (req.session.data['cya'] === 1) {
+        res.redirect('/user/check')
+    } else {
+        res.redirect('/user/page7')
+    }
+
+});
+router.post('/user/page7', function (req, res) {
+    if (req.session.data['cya'] === 1) {
+        res.redirect('/user/check')
+    } else {
+        res.redirect('/user/page8')
+    }
+
+});
+router.post('/user/page8', function (req, res) {
+    res.redirect('/user/check')
+});
+
+router.get('/user/check', function (req, res) {
+    req.session.data['cya'] = 1
+    res.render('user/check')
+});
+
+router.post('/user/check', function (req, res) {
+    res.redirect('/user/complete')
+});
+
+
+
+router.get('/user/check', function (req, res) {
+    req.session.data['cya'] = 1
+    res.render('user/check')
+});
+
+router.post('/user/retrieve', function (req, res) {
+    if (req.session.data['ref'] === '973HG2ID' && req.session.data['code'] === '87F34VD2') {
+        res.redirect('/user/report')
+    } else {
+        res.redirect('/user/notfound')
+    }
+});
+
+router.post('/user/notfound', function (req, res) {
+    if (req.session.data['ref'] === '973HG2ID' && req.session.data['code'] === '87F34VD2') {
+        res.redirect('/user/report')
+    } else {
+        res.redirect('/user/notfound')
+    }
+});
+
+
+
+
+
+router.get('/admin/dashboard', (req, res) => {
+    var data = require('./data/data.json')
+  
+    res.render('admin/dashboard', {
+      data
+    })
+  })
 
 module.exports = router
